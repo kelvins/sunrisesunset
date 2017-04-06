f439a8b7f2f3a8da4df237ff1115e3ffb6cfe7af
Kelvin S. do Prado <kelvinpfw@hotmail.com>
2017-04-06 11:49:37 -0300

    Applied gofmt

--- sunrisesunset_test.go
+++ sunrisesunset_test.go
@@ -19,14 +19,14 @@ func TestGetSunriseSunset(t *testing.T) {
 		date          time.Time
 		expectedError string
 	}{
-		{     -95.0, -46.704082,  -3.0, date, "Latitude invalid"},
-		{     100.0, -46.704082,  -3.0, date, "Latitude invalid"},
-		{-23.545570,     -185.0,  -3.0, date, "Longitude invalid"},
-		{-23.545570,      190.0,  -3.0, date, "Longitude invalid"},
+		{-95.0, -46.704082, -3.0, date, "Latitude invalid"},
+		{100.0, -46.704082, -3.0, date, "Latitude invalid"},
+		{-23.545570, -185.0, -3.0, date, "Longitude invalid"},
+		{-23.545570, 190.0, -3.0, date, "Longitude invalid"},
 		{-23.545570, -46.704082, -15.0, date, "UTC offset invalid"},
-		{-23.545570, -46.704082,  18.0, date, "UTC offset invalid"},
-		{-23.545570, -46.704082,  -3.0, time.Date(1000, 1, 1, 0, 0, 0, 0, time.UTC), "Date invalid"},
-		{-23.545570, -46.704082,  -3.0, time.Date(3000, 1, 1, 0, 0, 0, 0, time.UTC), "Date invalid"},
+		{-23.545570, -46.704082, 18.0, date, "UTC offset invalid"},
+		{-23.545570, -46.704082, -3.0, time.Date(1000, 1, 1, 0, 0, 0, 0, time.UTC), "Date invalid"},
+		{-23.545570, -46.704082, -3.0, time.Date(3000, 1, 1, 0, 0, 0, 0, time.UTC), "Date invalid"},
 	}
 
 	// Test with all values in the table
@@ -51,9 +51,9 @@ func TestGetSunriseSunset(t *testing.T) {
 		sunset    time.Time
 	}{
 		{-23.545570, -46.704082, -3.0, date, time.Date(1, 1, 1, 6, 11, 44, 0, time.UTC), time.Date(1, 1, 1, 18, 14, 27, 0, time.UTC)}, // Sao Paulo - Brazil
-		{36.7201600, -4.4203400,  1.0, date, time.Date(1, 1, 1, 7, 16, 45, 0, time.UTC), time.Date(1, 1, 1, 19, 32, 10, 0, time.UTC)}, // Málaga - Spain
-		{ 28.613084,  77.209168,  5.5, date, time.Date(1, 1, 1, 6, 21, 45, 0, time.UTC), time.Date(1, 1, 1, 18, 34, 07, 0, time.UTC)}, // Nova Delhi - India
-		{ 32.755701, -96.797296, -5.0, date, time.Date(1, 1, 1, 7, 26, 34, 0, time.UTC), time.Date(1, 1, 1, 19, 41, 07, 0, time.UTC)}, // Dallas - USA
+		{36.7201600, -4.4203400, 1.0, date, time.Date(1, 1, 1, 7, 16, 45, 0, time.UTC), time.Date(1, 1, 1, 19, 32, 10, 0, time.UTC)},  // Málaga - Spain
+		{28.613084, 77.209168, 5.5, date, time.Date(1, 1, 1, 6, 21, 45, 0, time.UTC), time.Date(1, 1, 1, 18, 34, 07, 0, time.UTC)},    // Nova Delhi - India
+		{32.755701, -96.797296, -5.0, date, time.Date(1, 1, 1, 7, 26, 34, 0, time.UTC), time.Date(1, 1, 1, 19, 41, 07, 0, time.UTC)},  // Dallas - USA
 	}
 
 	// Test with all values in the table
@@ -93,13 +93,13 @@ func TestDifferentLength(t *testing.T) {
 	var tTests = []struct {
 		result []float64
 	}{
-		{ calcSunEqCtr(slice1, slice2)                       },
-		{ calcSunTrueLong(slice1, slice2)                    },
-		{ calcSunAppLong(slice1, slice2)                     },
-		{ calcObliqCorr(slice1, slice2)                      },
-		{ calcSunDeclination(slice1, slice2)                 },
-		{ calcEquationOfTime(slice1, slice2, slice2, slice2) },
-		{ calcEquationOfTime(slice2, slice2, slice2, slice1) },
+		{calcSunEqCtr(slice1, slice2)},
+		{calcSunTrueLong(slice1, slice2)},
+		{calcSunAppLong(slice1, slice2)},
+		{calcObliqCorr(slice1, slice2)},
+		{calcSunDeclination(slice1, slice2)},
+		{calcEquationOfTime(slice1, slice2, slice2, slice2)},
+		{calcEquationOfTime(slice2, slice2, slice2, slice1)},
 	}
 
 	// Test with all values in the table
@@ -135,12 +135,12 @@ func TestRound(t *testing.T) {
 		parameter float64
 		result    int
 	}{
-		{ -0.002,  0 },
-		{ -0.510, -1 },
-		{ -4.290, -4 },
-		{  0.490,  0 },
-		{  0.510,  1 },
-		{ 10.280, 10 },
+		{-0.002, 0},
+		{-0.510, -1},
+		{-4.290, -4},
+		{0.490, 0},
+		{0.510, 1},
+		{10.280, 10},
 	}
 
 	// Test with all values in the table
