7786972cf0bbc654aa1bc3b5bf0b5031f79daf5b
Lukas Schwab <lukas.schwab@gmail.com>
2021-02-17 09:32:37 -0800

    Splice sunrise/sunset times onto date parameter
    Reuses the date parameter's year, month, day, and location in the output
    times instead of indicating the times on the date 1/1/1 in UTC: `new(time.Time)`.

--- sunrisesunset_test.go
+++ sunrisesunset_test.go
@@ -64,10 +64,10 @@ func TestGetSunriseSunset(t *testing.T) {
 		sunrise   time.Time
 		sunset    time.Time
 	}{
-		{-23.545570, -46.704082, -3.0, date, time.Date(1, 1, 1, 6, 11, 44, 0, time.UTC), time.Date(1, 1, 1, 18, 14, 27, 0, time.UTC)}, // Sao Paulo - Brazil
-		{36.7201600, -4.4203400, 1.0, date, time.Date(1, 1, 1, 7, 16, 45, 0, time.UTC), time.Date(1, 1, 1, 19, 32, 10, 0, time.UTC)},  // Málaga - Spain
-		{28.613084, 77.209168, 5.5, date, time.Date(1, 1, 1, 6, 21, 45, 0, time.UTC), time.Date(1, 1, 1, 18, 34, 07, 0, time.UTC)},    // Nova Delhi - India
-		{32.755701, -96.797296, -5.0, date, time.Date(1, 1, 1, 7, 26, 34, 0, time.UTC), time.Date(1, 1, 1, 19, 41, 07, 0, time.UTC)},  // Dallas - USA
+		{-23.545570, -46.704082, -3.0, date, time.Date(2017, 3, 23, 6, 11, 44, 0, time.UTC), time.Date(2017, 3, 23, 18, 14, 27, 0, time.UTC)}, // Sao Paulo - Brazil
+		{36.7201600, -4.4203400, 1.0, date, time.Date(2017, 3, 23, 7, 16, 45, 0, time.UTC), time.Date(2017, 3, 23, 19, 32, 10, 0, time.UTC)},  // Málaga - Spain
+		{28.613084, 77.209168, 5.5, date, time.Date(2017, 3, 23, 6, 21, 45, 0, time.UTC), time.Date(2017, 3, 23, 18, 34, 07, 0, time.UTC)},    // Nova Delhi - India
+		{32.755701, -96.797296, -5.0, date, time.Date(2017, 3, 23, 7, 26, 34, 0, time.UTC), time.Date(2017, 3, 23, 19, 41, 07, 0, time.UTC)},  // Dallas - USA
 	}
 
 	// Test with all values in the table
